151c4591346714dcdcf31355f0589beded5dbf26
YunxiuXu <60459891+YunxiuXu@users.noreply.github.com>
2023-01-10 22:17:42 +0900

    added Hand

--- Assets/Scripts/staticOnHandPresent.cs
+++ Assets/Scripts/staticOnHandPresent.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//在手刚出现的时候让物体Static几秒钟
+//在手刚出现的时候让物体Static几秒钟 防止乱蹦
 public class staticOnHandPresent : MonoBehaviour
 {
-    public GameObject RightHand;
+    public GameObject oneHand;
+    bool wasActive;
     // Start is called before the first frame update
+    void Awake()
+    {
+        wasActive = oneHand.activeSelf;
+    }
     void Start()
     {
         
@@ -15,6 +20,21 @@ public class staticOnHandPresent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        print(RightHand.activeSelf);
+        if (oneHand.activeSelf && !wasActive)
+        {
+            for (int i = 0; i < this.gameObject.transform.childCount; i++)
+            {
+                Transform child = this.gameObject.transform.GetChild(i);
+                //print(child.name);
+                if(child.GetComponent<PHSolidBehaviour>() != null){
+                    //print(child.name);
+                    //print(child.GetComponent<PHSolidBehaviour>().GetDescStruct());
+                    //child.GetComponent<PHSolidBehaviour>().ApplyDesc(child.GetComponent<PHSolidBehaviour>().GetDescStruct(), child.GetComponent<PHSolidBehaviour>().GetDescStruct());
+                }
+            }
+            Debug.Log("objectActive");
+        }
+
+        wasActive = oneHand.activeSelf;
     }
 }
