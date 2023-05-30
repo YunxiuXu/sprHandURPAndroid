0624316f02d9ec7f9624347b7dfb2f4a9bbea545
YunxiuXu <60459891+YunxiuXu@users.noreply.github.com>
2023-05-30 18:28:28 +0900

    增加物体掉落自动恢复和环形物体

---
+++ Assets/Scripts/objectRespawn.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//负责管理物体掉光了自动生成的
+
+public class objectRespawn : MonoBehaviour
+{
+    public List<Color> colorlist;
+
+    //public float deadY;
+    float deadY = -1;
+    Vector3 initPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initPosition = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(this.transform.position.y < deadY){
+            GameObject go = Instantiate(this.gameObject, initPosition, new Quaternion(0, 0, 0, 1));
+            //go.GetComponent<Renderer> ().material.color = colorlist[Random.Range(0, colorlist.Count)]; 
+            Destroy(this.gameObject);
+        }
+        
+    }
+}
